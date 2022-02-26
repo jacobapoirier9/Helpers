3d6fe64a15091ff1c363b70f8f5171d62fb7f7d4
jacobapoirier9 <64926220+jacobapoirier9@users.noreply.github.com>
2022-02-26 11:41:33 -0500

    Added DateTimeHelpers with tests

---
+++ Helpers.Tests/DateTimeHelpersTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Xunit;
+
+namespace Helpers.Tests
+{
+    public class DateTimeHelpersTests
+    {
+        private readonly DateTime _yesterday = DateTime.Now.AddDays(-1);
+        private readonly DateTime _today = DateTime.Now;
+        private readonly DateTime _tomorrow = DateTime.Now.AddDays(1);
+
+        [Fact]
+        public void IsBeforeOrOnTest()
+        {
+            Assert.True(DateTimeHelpers.IsBeforeOrOn(_yesterday, _today));
+            
+        }
+
+        [Fact]
+        public void IsAfterOrOnTest()
+        {
+            Assert.True(DateTimeHelpers.IsAfterOrOn(_tomorrow, _today));
+            
+        }
+
+        [Fact]
+        public void IsOnTest()
+        {
+            Assert.True(DateTimeHelpers.IsOn(_today, _today));
+            
+        }
+
+        [Fact]
+        public void IsNotOnSameDayAsTest()
+        {
+            Assert.True(DateTimeHelpers.IsNotOnSameDayAs(_yesterday, _tomorrow));
+            
+        }
+
+        [Fact]
+        public void IsTodayTest()
+        {
+            Assert.True(DateTimeHelpers.IsToday(_today));
+        }
+
+        [Fact]
+        public void IsTomorrowTest()
+        {
+            Assert.True(DateTimeHelpers.IsTomorrow(_tomorrow));
+        }
+
+        [Fact]
+        public void IsYesterdayTest()
+        {
+            Assert.True(DateTimeHelpers.IsYesterday(_yesterday));
+        }
+
+        [Fact]
+        public void IsBeforeTest()
+        {
+            Assert.True(DateTimeHelpers.IsBefore(_yesterday, _today));
+        }
+
+        [Fact]
+        public void IsAfterTest()
+        {
+            Assert.True(DateTimeHelpers.IsAfter(_tomorrow, _today));
+        }
+
+        [Fact]
+        public void IsBetweenTest()
+        {
+            Assert.True(DateTimeHelpers.IsBetween(_today, _yesterday, _tomorrow));
+            Assert.True(DateTimeHelpers.IsBetween(_today, _tomorrow, _yesterday));
+        }
+
+        [Fact]
+        public void IsNotBetweenTest()
+        {
+            Assert.True(DateTimeHelpers.IsNotBetween(_tomorrow, _today, _yesterday));
+        }
+
+        [Fact]
+        public void IsDefaultTest()
+        {
+            Assert.False(DateTimeHelpers.IsDefault(_today));
+            Assert.True(DateTimeHelpers.IsDefault(default(DateTime)));
+        }
+    }
+}
---
+++ Helpers/DateTimeHelpers.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers
+{
+    /// <summary>
+    /// A collection of easy to read DateTime operations
+    /// </summary>
+    public static class DateTimeHelpers
+    {
+        /// <summary>
+        /// Determines whether a date is before or on a given date
+        /// </summary>
+        public static bool IsBeforeOrOn(this DateTime current, DateTime target) =>
+            current.IsBefore(target) || current.IsOn(target);
+
+        /// <summary>
+        /// Determines whether a date is after or on a given date
+        /// </summary>
+        public static bool IsAfterOrOn(this DateTime current, DateTime target) =>
+            current.IsAfter(target) || current.IsOn(target);
+
+        /// <summary>
+        /// Determines whether a date is on a given date
+        /// </summary>
+        public static bool IsOn(this DateTime current, DateTime target) => current.Date == target.Date;
+
+        /// <summary>
+        /// Determines whether a date is not on a given date
+        /// </summary>
+        public static bool IsNotOnSameDayAs(this DateTime current, DateTime target) => current.Date != target.Date;
+
+        /// <summary>
+        /// Determines whether a date is today
+        /// </summary>
+        public static bool IsToday(this DateTime current) => current.IsOn(DateTime.Today);
+
+        /// <summary>
+        /// Determines whether a date is tomorrow
+        /// </summary>
+        public static bool IsTomorrow(this DateTime current) => current.IsOn(DateTime.Today.AddDays(1));
+
+        /// <summary>
+        /// Determines whether a date is yesterday
+        /// </summary>
+        public static bool IsYesterday(this DateTime current) => current.IsOn(DateTime.Today.AddDays(-1));
+
+        /// <summary>
+        /// Determines whether a date is before a given date
+        /// </summary>
+        public static bool IsBefore(this DateTime current, DateTime target) => current < target;
+
+        /// <summary>
+        /// Determines whether a date is after a given date
+        /// </summary>
+        public static bool IsAfter(this DateTime current, DateTime target) => current > target;
+
+        /// <summary>
+        /// Determines whether a date is between either of the two given dates
+        /// </summary>
+        public static bool IsBetween(this DateTime current, DateTime targetOne, DateTime targetTwo)
+        {
+            if (targetOne.IsBefore(targetTwo))
+                return (current.IsBefore(targetTwo) && current.IsAfter(targetOne));
+            else
+                return (current.IsBefore(targetOne) && current.IsAfter(targetTwo));
+        }
+
+        /// <summary>
+        /// Determines whether a date is not between either of the two given dates
+        /// </summary>
+        public static bool IsNotBetween(this DateTime current, DateTime targetOne, DateTime targetTwo) =>
+            !current.IsBetween(targetOne, targetTwo);
+
+        /// <summary>
+        /// Determines whether a date is the default value
+        /// </summary>
+        public static bool IsDefault(this DateTime current) => current == default(DateTime);
+    }
+}
